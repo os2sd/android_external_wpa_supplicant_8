1344b111007fde39098aeb5e4e905a1d455b48e9
Srinivas Girigowda <sgirigow@qca.qualcomm.com>
2014-08-28 11:00:47 -0700

    Add printf NULL checks to silence static analyzer
    Add NULL checks to take care of issues reported by static analyzer tool
    on potentially using NULL with printf format %s (which has undefined
    behavior even though many compilers end up printing "(null)").
    
    Git-commit: db9418bb1b8054b54d9b299e0334850d5e2b77ad
    Git-repo : git://w1.fi/srv/git/hostap.git
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    
    Change-Id: Iccf18c97617aa3ec4b5b59cbe75ce08aeb37d152
    CRs-Fixed: 716166

--- hostapd/hostapd_cli.c
+++ hostapd/hostapd_cli.c
@@ -744,6 +744,8 @@ static int hostapd_cli_cmd_interface(struct wpa_ctrl *ctrl, int argc,
 	hostapd_cli_close_connection();
 	free(ctrl_ifname);
 	ctrl_ifname = strdup(argv[0]);
+	if (ctrl_ifname == NULL)
+		return -1;
 
 	if (hostapd_cli_open_connection(ctrl_ifname)) {
 		printf("Connected to interface '%s.\n", ctrl_ifname);
